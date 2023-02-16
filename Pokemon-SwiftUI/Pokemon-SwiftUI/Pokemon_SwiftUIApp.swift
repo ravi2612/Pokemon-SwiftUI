25ff688726bfdda91ee73a538849b8d89518abf4
raviNavarro1226 <ravi.navarro@madeinweb.app>
2023-02-15 21:26:00 -0300

    first commit start Project

---
+++ Pokemon-SwiftUI/Pokemon-SwiftUI/Pokemon_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_SwiftUIApp.swift
+//  Pokemon-SwiftUI
+//
+//  Created by Ravi navarro on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
